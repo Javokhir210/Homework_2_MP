ddfb1c2f7336a9be794b01332325743e29071f65
Javokhir Jumaboev <jumaboyevjavokhir@gmail.com>
2023-11-13 22:57:21 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Here is the explanation:
+
+Task 1 (Stateless):
+In the task 1 I have written 2 messages to the constructor of My class while calling it in the main function. 
+To check both you may comment out the one and try the next one. Now only second one will work as the first
+one is commented out.
+
+Task 2(Stateful and Widget Tree):
+In this task I have created a stateful widget that displays Fibonacci number series one by one. To run 
+it you should comment out the part that is calling Task 1, and call Task 2. 
+
+
+
+
+
+
+
+# widgets_practice
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'WidgetsPractice',
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  FirstPageState createState() => FirstPageState();
+}
+
+class FirstPageState extends State<MainPage> {
+  bool isTask1Selected = true;
+
+  void toggleTask() {
+    setState(() {
+      isTask1Selected = !isTask1Selected;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget Practice'),
+      ),
+      body: isTask1Selected ? Task1Page() : Task2Page(toggleTask),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleTask,
+        child: const Icon(Icons.swap_horiz),
+      ),
+    );
+  }
+}
+
+class Task1Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return Task1('Hello everyone, It is my Greetings');
+    return const Task1('Hello everyone, It is my Greetings');
+  }
+}
+
+class Task2Page extends StatelessWidget {
+  final VoidCallback toggleTask;
+
+  const Task2Page(this.toggleTask, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Task2(toggleTask: toggleTask);
+  }
+}
+
+class Task1 extends StatelessWidget {
+  // Task 1 class
+  final String greeting;
+
+  const Task1(this.greeting, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WidgetsPractice',
+      home: Scaffold(
+        backgroundColor: Colors.black26,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: const Text(
+            'Stateless Widget',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Text(
+            greeting,
+            style: const TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Task2 extends StatefulWidget {
+  const Task2({super.key, required VoidCallback toggleTask});
+
+  @override
+  Task2State createState() => Task2State();
+}
+
+class Task2State extends State<Task2> {
+  int count = 1, a = 1;
+
+  void changeNum() {
+    setState(() {
+      int b;
+      b = a;
+      a = count;
+      count += b;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WidgetsPractice',
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          title: const Text('Stateful Widget'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'The Current Fibonacci Number is :',
+                style: TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                '$count',
+                style: const TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: changeNum,
+                    tooltip: 'Press to see the next number',
+                    backgroundColor: Colors.black54,
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Icon(Icons.skip_next),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
